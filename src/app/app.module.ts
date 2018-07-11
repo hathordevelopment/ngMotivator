8f7f82148d06dfb95f201ad4848e9af7baf86064
Alexis Goupillot <hathor.development@gmail.com>
2018-07-11 18:57:47 +0200

    Added Firebase Connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule, FirebaseAppConfig } from 'angularFire2';
+import { AngularFireDatabaseModule } from 'angularFire2/database';
+
 import { AppComponent } from './app.component';
+
+const CONFIG: FirebaseAppConfig = {
+  apiKey: "AIzaSyC35-3FsUBO3vOviMfLJ5rzTcAv-cXe84Q",
+  authDomain: "ngmotivatordb.firebaseapp.com",
+  databaseURL: "https://ngmotivatordb.firebaseio.com",
+  projectId: "ngmotivatordb",
+  storageBucket: "ngmotivatordb.appspot.com",
+  messagingSenderId: "297731174004"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(CONFIG),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
